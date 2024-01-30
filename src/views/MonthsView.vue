d7eccf641ae142f31007009c36706612864d76e7
Kaia <13533828+Lawlheart@users.noreply.github.com>
2024-01-29 19:31:42 -0800

    make month page with confidant, basic

---
+++ src/views/MonthsView.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import AprilData from '../../data/months/April.json'
+import MayData from '../../data/months/May.json'
+import JuneData from '../../data/months/June.json'
+import JulyData from '../../data/months/July.json'
+import AugustData from '../../data/months/August.json'
+import SeptemberData from '../../data/months/September.json'
+import OctoberData from '../../data/months/October.json'
+import NovemberData from '../../data/months/November.json'
+
+const months = ref([
+  {
+    name: 'April',
+    data: AprilData
+  },
+  {
+    name: 'May',
+    data: MayData
+  },
+  {
+    name: 'June',
+    data: JuneData
+  },
+  {
+    name: 'July',
+    data: JulyData
+  },
+  {
+    name: 'August',
+    data: AugustData
+  },
+  {
+    name: 'September',
+    data: SeptemberData
+  },
+  {
+    name: 'October',
+    data: OctoberData
+  },
+  {
+    name: 'November',
+    data: NovemberData
+  }
+]);
+
+const timeSlots = ref([
+  "Daytime",
+  "After School",
+  "Evening"
+])
+
+</script>
+
+<template>
+  <div class="months">
+    <div class="month" v-for="month in months" :key="month.name">
+      <h2>{{ month.name }}</h2>
+      <section class="days">
+        <div class="day" v-for="day in month.data" :key="day.date">
+          <h3>{{ day.day }} {{ day.date }}</h3>
+          <!-- <p>{{ day.description }}</p> -->
+          <ng-container v-for="slot in timeSlots" :key="slot">
+            <section class="time-item" v-if="day[slot]">
+              <h4>{{ slot }} - {{ day[slot].weather }}</h4>
+              <div class="confidants">
+                <div v-for="confidant in day[slot].confidants.split('\n')">{{ confidant }}</div>
+              </div>
+              
+            </section>
+          </ng-container>
+
+        </div>
+      </section>
+      
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .months {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+}
+
+.day {
+  display: flex;
+  flex-direction: row;
+  padding: 0.5rem 1rem;
+  border: 1px solid #444
+}
+
+.time-item {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  padding: 1rem 0.5rem 2rem;
+  border: 1px solid #444
+}
+
+.confidants {
+  display: flex;
+  flex-direction: column;
+  margin-left: 1rem;
+}
+</style>
